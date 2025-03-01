63fb0099db997283f61151e115725f2a2769c9b5
darkc <darkc@darkc.com>
2025-03-01 11:25:42 +0100

    fix wsgi.ini

--- filmoteca/config/default_headers.py
+++ filmoteca/config/default_headers.py
@@ -2,17 +2,17 @@ URL_PROXY_CHECK = 'https://api.ipify.org/'
 
 HEADERS_DEFAULT = {
 	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0',
-	'Accept': 'application/json,text/plain,text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'Accept-Language': 'en-US,en;q=0.5',
-	'Accept-Encoding': 'gzip, deflate, br',
-	'X-Requested-With': 'XMLHttpRequest',
+#	'Accept': 'application/json,text/plain,text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#	'Accept-Language': 'en-US,en;q=0.5',
+#	'Accept-Encoding': 'gzip, deflate, br',
+#	'X-Requested-With': 'XMLHttpRequest',
 
 }
 
 HEADERS = {
 	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0',
-	'Accept': 'application/json,text/plain,text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	'Accept-Language': 'en-US,en;q=0.5',
-	'Accept-Encoding': 'gzip, deflate, br',
-	'X-Requested-With': 'XMLHttpRequest',
+#	'Accept': 'application/json,text/plain,text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#	'Accept-Language': 'en-US,en;q=0.5',
+#	'Accept-Encoding': 'gzip, deflate, br',
+#	'X-Requested-With': 'XMLHttpRequest',
 }
